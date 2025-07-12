c0062ff7e0f97f4203aa921dbbaf139a7504437d
Condor-OfSol <sarcher.ofsol@googlemail.com>
2025-07-12 10:31:25 +0100

    characters now being counted as well as words

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import count_words
+from stats import count_words, count_characters
 
 
 def get_book_text(file_path):
@@ -8,6 +8,8 @@ def get_book_text(file_path):
 
 def main():
     num_words = count_words(get_book_text("books/frankenstein.txt"))
+    num_characters = count_characters(get_book_text("books/frankenstein.txt"))
     print(f"{num_words} words found in the document")
+    print(num_characters)
 
 main()
--- stats.py
+++ stats.py
@@ -4,3 +4,12 @@ def count_words(string_of_words):
     for word in words:
         count += 1
     return count
+
+def count_characters(string_of_words):
+    characters_count = {}
+    for character in string_of_words:
+        if character.lower() in characters_count:
+            characters_count.update({character.lower(): characters_count[character.lower()] + 1})
+        else:
+            characters_count[character.lower()] = 1
+    return characters_count
